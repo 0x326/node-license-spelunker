255addb8718a5c330fcfcd9b5c6865f3f2352c96
John Meyer <0x326@users.noreply.github.com>
2017-01-10 21:07:26 -0500

    Add yargs; Upgrade to ES6; Add Java Doc comments; Rename Variables

--- index.js
+++ index.js
@@ -1,144 +1,197 @@
 #!/usr/bin/env node
-var path = require('path')
-  , fs = require('fs')
-  , async = require('async')
-  ;
 
+const path = require('path');
+const fs = require('fs');
+const async = require('async');
+const argv = require('yargs')
+  .usage('Usage: $0 [dir/] -o [outputFile]')
+  .option('output', {
+      alias: 'o',
+      describe: 'Output file'
+    })
+  .count('verbose')
+  .alias('v', 'verbose')
+  .argv;
 
-var projPath = path.resolve(process.argv[2] || '.');
-console.log('Project Path', projPath);
-var topPkg = require(path.join(projPath, 'package.json'));
+const VERBOSE_LEVEL = argv.verbose;
+const INFO_LEVEL = 1;
+const DEBUG_LEVEL = 2;
 
-var modules = [];
-var count = 0;
+let rootProjectPath;
+if (argv.length > 0) {
+  rootProjectPath = path.resolve(argv._[0]);
+}
+else {
+  rootProjectPath = path.resolve('./');
+}
+if (VERBOSE_LEVEL >= 0) {
+  console.log('Project Path', rootProjectPath);
+}
+let rootProjectPackage = require(path.join(rootProjectPath, 'package.json'));
 
-doLevel(projPath);
+let modules = [];
+let recursiveLevel = 0;
 
-
-function doLevel(nodePath) {
-  var pkg = require(path.join(nodePath, 'package.json'));
-  var nodeModulesPath = path.join(nodePath, 'node_modules');
-  count ++;
-
-  //console.log('package.json license', pkg.license);
+/**
+ * Recursively explores the dependencies of a given package
+ * @param {Path} packagePath The path to the package to inspect
+ * @return undefined
+ */
+function exploreDependencies(packagePath) {
+  let package = require(path.join(packagePath, 'package.json'));
+  let nodeModulesPath = path.join(packagePath, 'node_modules');
+  recursiveLevel++;
+  
+  if (VERBOSE_LEVEL >= DEBUG_LEVEL) {
+    console.log('package.json license', package.license);
+  }
 
   fs.exists(nodeModulesPath, function (dirExists) {
     if (dirExists) {
       fs.readdir(nodeModulesPath, function (err, files) {
-        if (err) throw err;
-        var directories = [];
+        if (err) {
+          throw err;
+        }
+        let directories = [];
 
-        files = files.map(function (f) { return path.join(nodeModulesPath, f); })
-        async.filter(files, isModuleDirectory, function(directories){
-          //console.log('module directories', directories);
-          directories.forEach(doLevel);
+        files = files.map(f => path.join(nodeModulesPath, f));
+        async.filter(files, isPackage, function(directories) {
+          if (VERBOSE_LEVEL >= DEBUG_LEVEL) {
+            console.log('module directories', directories);
+          }
+          directories.forEach(exploreDependencies);
         });
-      })
+      });
     }
-  })
+  });
 
-  licenseText(nodePath, function (license) {
+  findLicenseText(packagePath, function (license) {
     modules.push({
-      name: pkg.name,
-      version: pkg.version,
-      url: 'https://www.npmjs.com/package/' + pkg.name,
-      localPath: path.relative(projPath,nodePath),
-      pkgLicense: pkg.license,
+      name: package.name,
+      version: package.version,
+      url: 'https://www.npmjs.com/package/' + package.name,
+      localPath: path.relative(rootProjectPath, packagePath),
+      pkgLicense: package.license,
       license: license
     });
-    count--;
+    recursiveLevel--;
 
-    if (count == 0) {
-      var noLicenseFile = modules.filter(function (m) { return m.license === 'NO LICENSE FILE' });
-      var andNoPkgJsonLicense = noLicenseFile.filter(function (m) { return !m.pkgLicense });
-      console.log('# LICENSE FILE REPORT FOR', topPkg.name);
+    if (recursiveLevel === 0) {
+      let improperlyLicensedModules = modules.filter(m => m.license === 'NO LICENSE FILE' );
+      let unlicensedModules = improperlyLicensedModules.filter(m => !m.pkgLicense );
+      console.log('# LICENSE FILE REPORT FOR', rootProjectPackage.name);
       console.log('');
-      console.log(modules.length + ' nested dependencies')
-      console.log(noLicenseFile.length +  ' without identifiable license text')
-      console.log(andNoPkgJsonLicense.length +  ' without even a package.json license declaration', '\n\n')
+      console.log(modules.length, 'nested dependencies')
+      console.log(improperlyLicensedModules.length, 'without identifiable license text')
+      console.log(unlicensedModules.length, 'without even a package.json license declaration', '\n\n')
       modules.forEach(function(m) {
         console.log('##', m.name);
         console.log('');
         console.log(m.name + '@' + m.version);
         console.log(m.url);
         console.log(m.localPath);
-        if (m.pkgLicense) console.log('From package.json license property:', JSON.stringify(m.pkgLicense));
+        if (m.pkgLicense) {
+          console.log('From package.json license property:', JSON.stringify(m.pkgLicense));
+        }
         console.log('');
         console.log(m.license);
         console.log('');
-      })
+      });
     }
-  })
-
-
+  });
 }
 
-function licenseText (nodePath, cb) {
-  var possibleLicensePaths = [ path.join(nodePath, 'LICENSE'),
-                               path.join(nodePath, 'LICENCE'),
-                               path.join(nodePath, 'LICENSE.md'),
-                               path.join(nodePath, 'LICENSE.txt'),
-                               path.join(nodePath, 'LICENSE-MIT'),
-                               path.join(nodePath, 'LICENSE-BSD'),
-                               path.join(nodePath, 'MIT-LICENSE.txt'),
-                               path.join(nodePath, 'Readme.md'),
-                               path.join(nodePath, 'README.md'),
-                               path.join(nodePath, 'README.markdown')];
+/**
+ * Searches for license text within a given package
+ * @param projectPath The path to the package which to search for a license
+ * @param {Function} callback A function accepting a String argument: the license text
+ * @return undefined
+ */
+function findLicenseText(projectPath, callback) {
+  let possibleLicensePaths = [
+    path.join(projectPath, 'LICENSE'),
+    path.join(projectPath, 'LICENCE'),
+    path.join(projectPath, 'LICENSE.md'),
+    path.join(projectPath, 'LICENSE.txt'),
+    path.join(projectPath, 'LICENSE-MIT'),
+    path.join(projectPath, 'LICENSE-BSD'),
+    path.join(projectPath, 'MIT-LICENSE.txt'),
+    path.join(projectPath, 'Readme.md'),
+    path.join(projectPath, 'README.md'),
+    path.join(projectPath, 'README.markdown')
+  ];
 
- var emptyState  = "NO LICENSE FILE";
+  let unlicense  = "NO LICENSE FILE";
 
-  async.reduceRight(possibleLicensePaths, emptyState, function (state, licensePath, reduceCb) {
-    var isAReadme = (licensePath.toLowerCase().indexOf('/readme') > 0);
+  async.reduceRight(possibleLicensePaths, unlicense, function (license, licensePath, callback) {
+    let isAReadme = (licensePath.toLowerCase().indexOf('/readme') > 0);
 
-    // if we already found a licnese, don't bother looking at READMEs
-    if (state !== emptyState && isAReadme) return reduceCb (null, state);
+    // If we already found a licnese, don't bother looking at READMEs
+    if (license !== unlicense && isAReadme) {
+      return callback(null, license);
+    }
 
+    // Read license file
     fs.exists(licensePath, function (exists) {
-      if (!exists) return reduceCb(null, state);
+      if (!exists) {
+        return callback(null, license);
+      }
       fs.readFile(licensePath, { encoding: 'utf8' }, function (err, text) {
-        if (err) return logError(err, reduceCb)(err, state);
+        if (err) {
+          console.error(err);
+          return (err, license);
+        }
 
         if (isAReadme) {
-          var match = text.match(/\n[# ]*license[ \t]*\n/i);
-          if (match) {
-            //console.log(match.input.substring(match.index))
-            return reduceCb (null, 'FROM README:\n' + match.input.substring(match.index));
+          let licenseExcerpt = text.match(/\n[# ]*license[ \t]*\n/i);
+          if (licenseExcerpt) {
+            if (VERBOSE_LEVEL >= DEBUG_LEVEL) {
+              console.log(licenseExcerpt.input.substring(licenseExcerpt.index));
+            }
+            return callback(null, 'FROM README:\n' + licenseExcerpt.input.substring(licenseExcerpt.index));
           }
           else {
-            return reduceCb(null, state);
+            // Nothing found in README
+            return callback(null, license);
           }
         }
-        else {
-          return reduceCb (null, text);
-        }
 
-
-        return reduceCb (null, text);
-      })
-
+        // Update with file text
+        return callback(null, text);
+      });
+      
     });
   }, function (err, license) {
-    if (err) return cb('ERROR FINDING LICENSE FILE ' + err );
-    cb (license);
+    if (err) {
+      return callback('ERROR FINDING LICENSE FILE ' + err );
+    }
+    callback(license);
   });
 }
 
-function isModuleDirectory (dirPath, cb) {
-  var pkgPath = path.join(dirPath, 'package.json');
+/**
+ * Determines whether the given path contains a module by testing the existence of a `package.json` file
+ * @param {String} dirPath The path to the directory
+ * @param {Function} callback A function which accepts a boolean argument.
+ *  It is true if the path points to a package (is a dir and contains a `package.json` file); False otherwise
+ * @return undefined
+ */
+function isPackage(dirPath, callback) {
+  let packageManifest = path.join(dirPath, 'package.json');
   fs.stat(dirPath, function (err, stat) {
-    if (err) return logError(err, cb)(false);
-
-    var isdir = stat.isDirectory();
-    if (isdir) {
-      fs.exists(pkgPath, cb);
+    let isModuleDir = false;
+    if (err) {
+      console.error(err);
     }
-    else {
-      cb(false);
+    else if (stat.isDirectory()) {
+      fs.access(packageManifest, fs.constants.F_OK, (err) => {
+        // If there is no error, then the directory must contain a module
+        isModuleDir = err === null;
+      });
     }
+    callback(isModuleDir);
   });
 }
 
-function logError(err, cb) {
-  console.log('ERROR', err);
-  return cb
-}
+// Start exploring the root project
+exploreDependencies(rootProjectPath);
